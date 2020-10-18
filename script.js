e0e4dab90f0d41abf858ae2ff2c8e4a64f47977c
NDPenaloza <ndpenaloza@gmail.com>
2020-10-17 20:38:03 -0700

    added upper, lower, number, and symbols characters arrays

--- script.js
+++ script.js
@@ -4,19 +4,7 @@ var lowercasePW = confirm("Do  you want lowercase letters in your password?");
 var numbersPW = confirm("Do you want numbers in your password?");
 var symbolsPW = confirm("Do  you want symbols in your password?");
 
-var symbols = ["", ""]
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var upperrcase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "{", "}", "~"];
